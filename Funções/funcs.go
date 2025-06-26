abfc61ba9e3cb7fca314ad96c7def77c22020bbc
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-06-26 12:27:32 -0400

    Mais sobre funções

--- Funções/funcs.go
+++ Funções/funcs.go
@@ -16,8 +16,20 @@ func mult(x int, y int) int {
 	return x * y
 }
 
+// Função começando com letra minúscula
+// Função PRIVADA
+// Só pode ser utilizada dentro do próprio pacote
+
 func printNome(nome string) string {
 	return nome
+}
+
+// Função começando com letra maiúscul
+// Função PÚBLICA
+// Pode ser utilizada em outros pacotes
+
+func PrintNomeCompleto(nome, sobrenome string) (string, string) {
+	return nome, sobrenome
 }
 
 func main() {
@@ -26,4 +38,8 @@ func main() {
 	fmt.Println(" O resultado da multiplicação é:", mult(42, 13))
 
 	fmt.Println(printNome("Steph"))
+
+	nome, _ := PrintNomeCompleto("Steph", "Silva")
+
+	fmt.Println("Nome:", nome)
 }
