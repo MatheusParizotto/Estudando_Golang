c6bbfc56299f70fdc297fc565480d49911ecf5cf
Matheus Parizotto <matheusparizotto29@gmail.com>
2025-06-30 16:51:52 -0400

    Loops com for

---
+++ Laços/loop.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += 1
+		fmt.Println(sum)
+	}
+
+	frutas := []string{"laranja", "maça", "banana", "uva", "kiwi"}
+	for _, fruta := range frutas {
+		fmt.Println(fruta)
+	}
+
+}
